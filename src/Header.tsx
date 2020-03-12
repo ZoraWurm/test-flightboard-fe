fdf558d0cd39f4c1bd32eee6f00d5f2e883c7791
ZoraWurm <62095489+ZoraWurm@users.noreply.github.com>
2020-03-12 15:34:44 +0100

    add Raumliste (#2)
    * add Raumliste
    
    * remove react-table

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,12 @@
-import React from "react";
+import React from 'react';
 import './Header.css';
 
 function Header() {
-    return(
-      <div className="Header">      
-        <h1>
-        Flightboard
-        </h1>
-      </div>
-
-    );
+  return (
+    <div className="Header">
+      <h1>Flightboard</h1>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/Room/room.component.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './room.component.css';
+
+type RoomProps = {
+  isFree: boolean;
+};
+
+function Room({ isFree }: RoomProps) {
+  return (
+    <div className="room">
+      <div>{isFree ? 'Open' : 'Busy'}</div>
+      <div>Raumnummer</div>
+      <div>Uhrzeit</div>
+      <div>Titel/Name</div>
+    </div>
+  );
+}
+
+export default Room;
