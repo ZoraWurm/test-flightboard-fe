4c65f662b453697bbf20065b323602e3468e117e
Zora Wurm <zora.wurm@maibornwolff.de>
2020-03-12 15:10:44 +0100

    add Raumliste

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from "react";
-import "./App.css";
-import Header from "./Header";
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Room from './Room/room.component';
 
 function App() {
+  const roomList: any[] = [
+    { id: 1, isFree: true },
+    { id: 2, isFree: true },
+    { id: 3, isFree: false },
+    { id: 4, isFree: true },
+  ];
+
   return (
     <div className="App">
-      <Header />
-      <div>Mainpage</div>
+      {roomList.map((room) => (
+        <Room key={room.id} isFree={room.isFree} />
+      ))}
     </div>
   );
 }
-
 export default App;
