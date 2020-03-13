8dd8f3a0891e4f1f8ce3bebc154d159647699cf1
Zora Wurm <zora.wurm@maibornwolff.de>
2020-03-13 09:55:58 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,21 @@ export function App() {
     { id: 2, isFree: true },
     { id: 3, isFree: false },
     { id: 4, isFree: true },
+    { id: 5, isFree: true },
+    { id: 6, isFree: true },
+    { id: 7, isFree: true },
+    { id: 8, isFree: true },
+    { id: 9, isFree: true },
   ];
 
   return (
     <div className="App">
       <Header />
-      {roomList.map((room) => (
-        <Room key={room.id} isFree={room.isFree} />
-      ))}
+      <div className="roomColor">
+        {roomList.map((room) => (
+          <Room key={room.id} isFree={room.isFree} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Room/room.component.tsx
+++ src/Room/room.component.tsx
@@ -8,10 +8,14 @@ type RoomProps = {
 export function Room({ isFree }: RoomProps) {
   return (
     <div className="room">
-      <div>{isFree ? 'Open' : 'Busy'}</div>
-      <div>Raumnummer</div>
-      <div>Uhrzeit</div>
-      <div>Titel/Name</div>
+      <div className={isFree === true ? 'roomIsOpen' : 'roomIsBusy'}>
+        <div className={isFree === true ? 'styleOpen' : 'styleBusy'}>
+          {isFree ? 'Open' : 'Busy'}
+        </div>
+        <div>Raumnummer</div>
+        <div>Uhrzeit</div>
+        <div>Titel/Name</div>
+      </div>
     </div>
   );
 }
