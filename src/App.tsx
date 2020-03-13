746e8bee5412424236be0beea9d750baa5371538
Zora Wurm <zora.wurm@maibornwolff.de>
2020-03-13 13:12:23 +0100

    mainpagestyle

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,15 @@ import React from 'react';
 import './App.css';
 import { Room } from './room/room.component';
 import { Header } from './header/header.component';
+import { RoomEntry } from './room/room.model';
 
 export function App() {
-  const roomList: any[] = [
-    { id: 1, isFree: true },
-    { id: 2, isFree: true },
-    { id: 3, isFree: false },
-    { id: 4, isFree: true },
-    { id: 5, isFree: true },
-    { id: 6, isFree: true },
-    { id: 7, isFree: true },
-    { id: 8, isFree: true },
-    { id: 9, isFree: true },
+  const roomList: RoomEntry[] = [
+    { id: 1, roomName: 'Grace Hopper', isFree: true, location: 'TH13' },
+    { id: 2, roomName: 'Ada Lovelance', isFree: true, location: 'TH13' },
+    { id: 3, roomName: 'Kreativlab', isFree: false, location: 'TH13' },
+    { id: 4, roomName: 'Sofazimmer', isFree: true, location: 'TH13' },
+    { id: 5, roomName: 'Prinzessinenzimmer', isFree: true, location: 'TH13' },
   ];
 
   return (
@@ -21,7 +18,13 @@ export function App() {
       <Header />
       <div className="roomColor">
         {roomList.map((room) => (
-          <Room key={room.id} isFree={room.isFree} />
+          <Room
+            key={room.id}
+            id={room.id}
+            roomName={room.roomName}
+            isFree={room.isFree}
+            location={room.location}
+          />
         ))}
       </div>
     </div>
--- src/Room/room.component.tsx
+++ src/Room/room.component.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import './room.component.css';
+import { RoomEntry } from './room.model';
 
-type RoomProps = {
-  isFree: boolean;
-};
-
-export function Room({ isFree }: RoomProps) {
+export function Room({ roomName, isFree }: RoomEntry) {
   return (
-    <div className="room">
-      <div className={isFree === true ? 'roomIsOpen' : 'roomIsBusy'}>
-        <div className={isFree === true ? 'styleOpen' : 'styleBusy'}>
-          {isFree ? 'Open' : 'Busy'}
-        </div>
-        <div>Raumnummer</div>
-        <div>Uhrzeit</div>
-        <div>Titel/Name</div>
+    <div className={isFree === true ? 'room roomIsOpen' : 'room roomIsBusy'}>
+      <div className={isFree === true ? 'styleOpen' : 'styleBusy'}>
+        {isFree ? 'Open' : 'Busy'}
       </div>
+      <div>{roomName}</div>
+      <div>Uhrzeit</div>
+      <div>Titel/Name</div>
     </div>
   );
 }
